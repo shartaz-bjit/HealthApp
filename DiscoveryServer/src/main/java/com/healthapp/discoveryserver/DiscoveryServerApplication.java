af0d3db7560a00fbedd65e621dab99a830ff3a88
shartaz-bjit <shartaz.yeasar@bjitacademy.com>
2023-09-30 12:08:05 +0600

    Discovery server, Config server, API Gateway integrated

--- DiscoveryServer/src/main/java/com/healthapp/discoveryserver/DiscoveryServerApplication.java
+++ DiscoveryServer/src/main/java/com/healthapp/discoveryserver/DiscoveryServerApplication.java
@@ -2,12 +2,12 @@ package com.healthapp.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
-
 }
