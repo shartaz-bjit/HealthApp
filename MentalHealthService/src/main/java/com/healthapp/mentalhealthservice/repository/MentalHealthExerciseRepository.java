95f5cf218242af126f02f0a6537c9a31026165a9
G M Mehedi Hasan <141901188+mehedibjit@users.noreply.github.com>
2023-10-02 04:42:14 +0600

    Revert "Merge branch 'development' into nutrition_service"
    This reverts commit da5e0b0d8532a8b30d9d56d2e3b1c7d205d59b78, reversing
    changes made to 85935916f8fb6e3f9073bc7a0b02826a1941f736.

--- MentalHealthService/src/main/java/com/healthapp/mentalhealthservice/repository/MentalHealthExerciseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.healthapp.mentalhealthservice.repository;
-
-import com.healthapp.mentalhealthservice.entity.MentalHealthExercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// MentalHealthExerciseRepository.java
-public interface MentalHealthExerciseRepository extends JpaRepository<MentalHealthExercise, Long> {
-}
